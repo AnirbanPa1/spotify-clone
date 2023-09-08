ff2dbedca6180645eac5fac9a08df7411a25ad1b
ANIRBAN <palanirban2003@gmail.com>
2023-09-08 22:40:00 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "dzydyjyewldkmtkoqmvs.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+    const [isMounted, setIsmounted] = useState(false);
+    
+    useEffect(() => {
+        setIsmounted(true);
+    } , [] );
+    
+    if(!isMounted) {
+        return null;
+    }
+    
+    return ( 
+        <>
+            <AuthModal />
+            <UploadModal />
+        </>
+     );
+}
+ 
+export default ModalProvider;
